45a178930982a623f896e065bda67db77a5e7488
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2022-12-29 12:22:12 -0800

    rearrange timeComplexityGauri order

--- timeComplexity/timeComplexityGauri.js
+++ timeComplexity/timeComplexityGauri.js
@@ -1,31 +1,31 @@
 let n = 10;
 
-// O(n^2)
+// O(n)
 let ctr1 = 0;
-for (let i = 0; i < n; i++) {
+for (let i = 0; i < 3; i++) {
   for (let j = 0; j < n; j++) {
     ctr1++;
   }
 }
-console.log(ctr1);
-
-// O(n)
-let ctr2 = 0;
-for (let i = 0; i < 3; i++) {
-  for (let j = 0; j < n; j++) {
-    ctr2++;
-  }
-}
-console.log(ctr2);
+console.log(ctr1);  // 30
 
 // O( n*log(n))
-let ctr3 = 0;
+let ctr2 = 0;
 for (let i = 0; i < n; i++) {       // linear: O(n)
   for (let j = 1; j < n; j *= 2) {  // logarithmic: O(log(n))
-    ctr3++;                         // constant: O(1)
+    ctr2++;                         // constant: O(1)
   }
 }
-console.log(ctr3);
+console.log(ctr2);  // 40
+
+// O(n^2)
+let ctr3 = 0;
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < n; j++) {
+    ctr3++;
+  }
+}
+console.log(ctr3); // 100
 
 // O(2^n)
 let ctr4 = 0;
@@ -35,4 +35,4 @@ function recursive(n) {
   return recursive(n - 1) + recursive(n - 1);
 }
 recursive(n);
-console.log(ctr4);
+console.log(ctr4); // 1023
